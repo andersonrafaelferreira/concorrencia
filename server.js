af9e8d463026a564365e0b8e5895d276a9182981
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2020-03-04 09:11:11 -0300

    add other pages

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const server = express()
+const server = express();
 
-server.use(express.static("public"))
+mongoose.connect(
+  "mongodb+srv://rafael:rafael4455@@@cluster0-umxyp.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
 
-const nunjucks = require("nunjucks")
+server.use(express.static("public"));
 
-nunjucks.configure("src",{
-    express: server,
-    noCache: true
-})
+const nunjucks = require("nunjucks");
 
+nunjucks.configure("src", {
+  express: server,
+  noCache: true
+});
+
+const oppos = [
+  {
+    id: 1,
+    status: "Aberta"
+  },
+  {
+    id: 2,
+    status: "Fechada"
+  },
+  {
+    id: 3,
+    status: "Cancelada"
+  }
+];
 
 server.get("/", (req, res) => {
-    res.render("abertas.html")
-})
+  res.render("abertas.html");
+});
+server.get("/abrir", (req, res) => {
+  res.render("nova.html");
+});
 
 server.get("/fechadas", (req, res) => {
-    res.render("fechadas.html")
-})
+  res.render("fechadas.html", { oppos });
+});
 
-server.listen(3000)
+server.listen(3000);
